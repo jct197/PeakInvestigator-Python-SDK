1835e5ee7adc88ef4494f18042d69ac414399cd2
Adam Tenderholt <atenderholt@gmail.com>
2016-10-23 16:29:31 -0700

    Add copyright headers

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,11 @@
+## -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016, Veritomyx, Inc.
+#
+# This file is part of the Python SDK for PeakInvestigator
+# (http://veritomyx.com) and is distributed under the terms
+# of the BSD 3-Clause license.
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(".."))
